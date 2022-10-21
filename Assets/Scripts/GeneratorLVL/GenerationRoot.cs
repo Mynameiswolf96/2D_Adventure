337e7d207a18c1ce263c496853f0a9662ecb6206
Slava Kuchuk <Mynameiswolf96>
2022-10-21 19:02:46 +0300

    homework6

---
+++ Assets/Scripts/GeneratorLVL/GenerationRoot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GenerationRoot: MonoBehaviour
+{
+    [SerializeField]
+    private GenerateLevelView _generateLevelView;
+
+    private GenerateLevelController _generateLevelController;
+
+    private void Awake()
+    {
+        _generateLevelController = new GenerateLevelController(_generateLevelView);
+        _generateLevelController.Awake();
+    }
+}
